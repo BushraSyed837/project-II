4d1b6b6077e9f7fa566854b6389168ceff9d5999
Bushra Syed <74993780+BushraSyed837@users.noreply.github.com>
2022-03-08 17:07:27 +0500

    updated

--- index.js
+++ index.js
@@ -5,50 +5,75 @@ fastify.register(require('fastify-mongodb'), {
   forceClose: true,
   url: 'mongodb://localhost:27017/project1'
 })
-fastify.get('/chat/:driver/:user', { websocket: true }, async function wsHandler (
+fastify.get('/chat', { websocket: true }, async function wsHandler (
   connection,
   req
 ) {
-  const chat = this.mongo.db.collection('chats')
   connection.socket.on('message', async message => {
-    var driverMessages = []
-    var userMessages = []
-    const msgs = JSON.parse(message)
-    msgs.createdAt = new Date()
-    delete msgs._id
-    await chat.insertOne(msgs)
+    connection.socket.send(message)
+    let allClients = this.websocketServer.clients
+    allClients.forEach(client => {
+      console.log(client.readyState === connection.socket.OPEN)
+      try {
         if (
-          msgs.driverID == req.params.driver &&
-          msgs.userID == req.params.user
+          client.readyState === connection.socket.OPEN
         ) {
-          if (msgs.type == 'send') {
-            connection.socket.send(`Message: ${msgs.messageContent}`)
-          } else {
-            const result = await chat
-              .find({
-                userID: msgs.userID,
-                driverID: msgs.driverID
-              })
-              .toArray()
-            console.log(result.length)
-
-            for (let i = 0; i < result.length; i++) {
-              if (result[i].senderType == 'driver') {
-                driverMessages.push(result[i].messageContent)
-              } else if (result[i].senderType == 'user') {
-                userMessages.push(result[i].messageContent)
-              }
-            }
-            userMessages.forEach(d => {
-              connection.socket.send(`User Message: ${d}`)
-            })
-            driverMessages.forEach(d => {
-              connection.socket.send(`Driver Message: ${d}`)
-            })
-          }
+          client.send(message)
         }
+      } catch (err) {
+        console.log(err)
+      }
+    })
   })
 })
+// fastify.get(
+//   '/chat/:driver/:user',
+//   { websocket: true },
+//   async function wsHandler (connection, req) {
+//     const chat = this.mongo.db.collection('chats')
+//     connection.socket.on('message', async message => {
+//       const msgs = JSON.parse(message)
+//       msgs.createdAt = new Date()
+//       delete msgs._id
+//       await chat.insertOne(msgs)
+//       if (
+//         msgs.driverID == req.params.driver &&
+//         msgs.userID == req.params.user
+//       ) {
+//         if (msgs.type == 'send') {
+//           connection.socket.send(`Message: ${msgs.messageContent}`)
+//         } else {
+//           const result = await chat
+//             .find({
+//               userID: msgs.userID,
+//               driverID: msgs.driverID
+//             })
+//             .toArray()
+//           const driverMessages = result.reduce((content, i) => {
+//             if (i.senderType == 'driver') {
+//               content.push(i.messageContent)
+//             }
+//             return content
+//           }, [])
+//           console.log('driver', driverMessages)
+//           const userMessages = result.reduce((content, i) => {
+//             if (i.senderType == 'driver') {
+//               content.push(i.messageContent)
+//             }
+//             return content
+//           }, [])
+//           console.log('user', userMessages)
+//           userMessages.forEach(d => {
+//             connection.socket.send(`User Message: ${d}`)
+//           })
+//           driverMessages.forEach(d => {
+//             connection.socket.send(`Driver Message: ${d}`)
+//           })
+//         }
+//       }
+//     })
+//   }
+// )
 
 fastify.listen(3000, (err, address) => {
   if (err) {
